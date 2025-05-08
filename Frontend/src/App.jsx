2c6266611304a639211b1a4798999ef01fbf9224
navaj bloch <navajbloch420@gmail.com>
2025-05-08 15:16:28 +0530

    set-up pages and routing and components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,24 @@
  import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+import Contact from './Pages/Contact'
+import About from './Pages/About'
+import Footer from './Components/Footer'
  
  function App() {
    return (
-     <div>App</div>
+     <div> 
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+     </div>
    )
  }
  
