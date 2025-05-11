03c4ce6c24092e7f191c5b2e44745a02e1fcf819
navaj bloch <navajbloch420@gmail.com>
2025-05-11 18:44:02 +0530

    fixing ui and image

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,35 @@
- import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar'
-import Home from './Pages/Home'
-import Contact from './Pages/Contact'
-import About from './Pages/About'
-import Footer from './Components/Footer'
-import Login from './Pages/Login'
-import SignUp from './Pages/SignUp'
-import Products from './Pages/Products'
- function App() {
+ import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+import About from './Pages/About';
+import Footer from './Components/Footer';
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';
+import Products from './Pages/Products';
+
+function App() {
   const user = JSON.parse(localStorage.getItem('loggedInUser'));
 
-   return (
-    
-      <BrowserRouter>
-      <Navbar/>
+  return (
+    <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
         <Route
-          path="/products"
-          element={user ? <Products /> : <Navigate to="/login" />}
+          path='/products'
+          element={user ? <Products /> : <Navigate to='/login' />}
         />
       </Routes>
-      <Footer/>
-      </BrowserRouter>
-    
-   )
- }
- 
- export default App
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- Frontend/src/assets/data.js
+++ Frontend/src/assets/data.js
@@ -1,12 +1,11 @@
-import coffee3 from '../assets/coffee 3.jpg';
-import coffee4 from '../assets/coffee 4.jpg';
-import coffee5 from '../assets/coffee 5.webp';
-import coffee6 from '../assets/coffee 6.webp';
-import coffee7 from '../assets/coffee 7.webp';
-import coffee8 from '../assets/coffee 8.webp';
-import coffee from '../assets/coffee.jpg';
-import coffee_new from '../assets/coffee.png';
-import coffee_new2 from '../assets/coffee.webp';
+import coffee3 from '../assets/Coffee 3.jpg';
+import coffee4 from '../assets/Coffee 4.jpg';
+import coffee5 from '../assets/Coffee 5.webp';
+import coffee6 from '../assets/Coffee 6.webp';
+import coffee7 from '../assets/Coffee 7.webp';
+import coffee8 from '../assets/Coffee 8.webp';
+import coffee_new from '../assets/Coffee.png';
+import coffee_new2 from '../assets/Coffee.webp';
 
 
  export const assets = {
@@ -16,8 +15,7 @@ import coffee_new2 from '../assets/coffee.webp';
     coffee6,
     coffee7,
     coffee8,
-    coffee,
-    coffee_new,
+     coffee_new,
     coffee_new2
 }
 
